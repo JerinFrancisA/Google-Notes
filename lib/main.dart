62a406ede7b4ceed5b8afff066236ee1b2c885be
JerinFrancisA <jerinfrancis77@gmail.com>
2019-09-06 17:29:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HomeScreen());
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gnotes',
+      initialRoute:,
+      routes: {
+
+      },
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SubjectsPage extends StatefulWidget {
+  @override
+  _SubjectsPageState createState() => _SubjectsPageState();
+}
+
+class _SubjectsPageState extends State<SubjectsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

100998757a00bf1fb5b0d28308a31135d0be9d6a
JerinFrancisA <jerinfrancis77@gmail.com>
2019-09-09 01:10:01 +0530

    splash & icon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -72,6 +72,7 @@ class _NotesPageState extends State<NotesPage> {
                           });
                         },
                       ),
+                      isThreeLine: true,
                     ),
                   );
                 },
